9e3487c6ebcbd29c28ec0588c5dfb23df51c4066
zhren-byte <chavez.thomas970@gmail.com>
2024-01-13 05:37:15 -0300

    Inicio de Spotify Steal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const fs = require("node:fs");
 const path = require("node:path");
 const express = require("express");
+const axios = require("axios");
 const app = express();
 const dotenv = require("dotenv");
+const fetch = require('isomorphic-unfetch')
+const { getData, getPreview, getTracks, getDetails } =
+  require('spotify-url-info')(fetch)
 dotenv.config();
 const port = process.env.PORT || 3000;
 
@@ -41,6 +45,32 @@ app.get("/", function (req, res) {
 app.get("/obtain-song", function (req, res) {
 	let song = songsInfo[Math.floor(Math.random() * songsInfo.length)];
 	res.json(song);
+});
+
+app.get("/spotify", function (req, res) {
+  const track = req.query.track
+	if(track) return axios
+		.get(`https://spclient.wg.spotify.com/color-lyrics/v2/track/${track}/image/https%3A%2F%2Fi.scdn.co%2Fimage%2Fab67616d0000b273833f2d8d0037a5ae87595fb0?format=json&vocalRemoval=false&market=from_token`, {
+			headers: {
+        "Accept": "application/json",
+        "Accept-Language": "es-419",
+        "App-Platform": "WebPlayer",
+         "Authorization": `Bearer ${process.env.Authorization}`,
+         "Client-Token": process.env.ClientToken,
+         "Referer": "https://open.spotify.com/",
+         "Sec-Ch-Ua": '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+         "Sec-Ch-Ua-Mobile":"?0",
+         "Sec-Ch-Ua-Platform": "Windows",
+         "Spotify-App-Version": "1.2.30.104.ge8490e8d",
+         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+      },
+		})
+		.then((response) => res.json(response.data))
+		.catch((err) => console.error(err));
+  getPreview('https://open.spotify.com/intl-es/track/1tW6LiJGXGlReuNP38wrKb?si=846143622bbd4d97').then(data =>
+    console.log(data)
+  )
+  return res.render("spotifyFinder")
 });
 
 app.get("/add-song", function (req, res) {
